17b7caafdb8c627a3a155ea32b92a8a93fbb90fa
John P Vajda <jvajda@newrelic.com>
2021-05-04 19:46:11 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# labelparse
-a simple python program to parse issues and labels from Github Projects
+# Label Parse
+
+`Label Parse` is a simple Python program to parse issues and labels from Github Projects.
+
+This program will only work with github projects that are public if you wish to
+query a private repo you can use a [Github PAT](https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token)
+to authenticate.
---
+++ labelparse.py
@@ -0,0 +1,39 @@
+import json
+import requests
+
+
+# sets reponames
+
+reponames = ['nr1-github', 'nr1-account-maturity', 'nr1-browser-analyzer', 'nr1-cloud-optimize',  'nr1-container-explorer', 'nr1-datalyzer', 'nr1-deployment-analyzer', 'nr1-event-stream', 'nr1-graphiql-notebook', 'nr1-groundskeeper', 'nr1-integrations-manager',
+             'nr1-learn-nrql', 'nr1-metrics-aggregator', 'nr1-neon', 'nr1-network-telemetry', 'nr1-nimbus', 'nr1-observability-maps', 'nr1-pageview-map', 'nr1-pathpoint', 'nr1-quickstarts', 'nr1-slo-r', 'nr1-status-pages', 'nr1-tag-improver', 'nr1-top', 'nr1-workload-geoops']
+
+# Loops through repo data
+
+
+def print_labels(issue):
+    if 'labels' in issue and issue['labels'] != []:
+        for label in issue['labels']:
+            print('Label: ' + label['name'])
+
+
+def print_issue(issue):
+    print('\n')
+    print('Repo: ' + issue['repository_url'])
+    print('ID: ' + str(issue['id']))
+    print('Title: ' + issue['title'])
+    print_labels(issue)
+
+# Fetches repo data
+
+
+for repo in reponames:
+    response = requests.get(
+        'https://api.github.com/repos/newrelic/' + str(repo) + '/issues')
+
+    for issue in response.json():
+        print_issue(issue)
+
+# for each issue
+#   for each label
+#     if we have a count for this label, +1
+#     if we don't have a count for this label, make one and set to 1
